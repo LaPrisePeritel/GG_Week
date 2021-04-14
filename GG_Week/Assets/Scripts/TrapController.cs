16a1470071b931fc5b902808d518d1ef2b4b473b
JulesFritz <jules.fritz@edu.devinci.fr>
2021-04-14 09:29:06 +0200

    add : plein de trucs (mardi)

---
+++ GG_Week/Assets/Scripts/TrapController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public GameObject[] trapArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckTraps();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckTraps();
+            foreach (GameObject item in trapArray)
+            {
+                item.GetComponent<Trap>().ChangeTrapState();
+            }
+        }
+    }
+
+    public void CheckTraps()
+    {
+        trapArray = GameObject.FindGameObjectsWithTag("Trap");
+    }
+}
